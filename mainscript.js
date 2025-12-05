ecb13fd745e05ca2d11ef570ddfc81b95377133c
Elunify <elunifyapp@gmail.com>
2025-12-05 18:42:18 +0100

    subabase back

--- mainscript.js
+++ mainscript.js
@@ -1,12 +1,7 @@
 //--------------------------
 // SUPABASE
 //--------------------------
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-// --- Initialize Supabase ---
-const supabaseUrl = 'https://pqrgvelzxmtdqrofxujx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxcmd2ZWx6eG10ZHFyb2Z4dWp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMTc0ODAsImV4cCI6MjA3MTY5MzQ4MH0.s8JZLDdzIS1wBLln0Zs3LK_9BHelUcbRhyAC_0-5sos';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { supabase } from './supabaseClient.js';
 
 import { LessonsByIndex } from './scriptpools.js';
 import { HealthIssuesPool } from './scriptpools.js';
